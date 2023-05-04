ba84ff2594b4bc041c08fb76284e15cf39123dcc
Aswanthachu <pnkaswanthachu546@gmail.com>
2023-05-04 10:12:39 +0530

    not post back button functionalities added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ export default function Home({ allPosts, posts, setPosts }) {
           )}
         </div>
       ) : (
-        <NoPostAvailable />
+        <NoPostAvailable setPosts={setPosts}/>
       )}
     </main>
   );
